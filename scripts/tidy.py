4689f3843a4479c556d2309b6706a1f7e768dc2c
Maciej Brencz <maciej.brencz@gmail.com>
2016-12-18 19:51:46 +0100

    Move tidy and Sphinx functionality to wbc module

--- scripts/tidy.py
+++ scripts/tidy.py
@@ -1,111 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """
 Clean the provided txt file before passing it to the indexer
 
-./tidy.py issue.txt > clean.txt
+This entry point can be used for tests purposes
+
+tidy issue.txt > clean.txt
 """
-from __future__ import unicode_literals, print_function
-
 import fileinput
-import logging
 import sys
-import re
 
-
-class TextTidy(object):
-    EMPTY_HEADING = '\x1f\x1d\x0b'
-    PAGE_NUMBER = '\x0c'
-    PAGE_BREAK = '\x1f\x1d'
-
-    PAGE_NUMBER_RE = re.compile(r'^[\s\d]+$')
-
-    def __init__(self, _in, chapter_break=''):
-        self._in = _in
-        self._logger = logging.getLogger(self.__class__.__name__)
-        self._chapter_break = chapter_break
-
-    def tidy(self, output=sys.stdout):
-        buf = ''
-
-        def _write(_line):
-            output.write(_line + "\n")
-
-        # do the stuff
-        for i, line in enumerate(self._in):
-            def _debug(msg):
-                self._logger.debug('{} : [{}] ({} chars)'.format(i+1, msg, len(msg)))
-
-            try:
-                line = line.decode('utf-8', 'ignore')
-            except UnicodeDecodeError:
-                self._logger.error('UTF-8 encoding error', exc_info=True)
-                print(line)
-
-            line = line.rstrip()
-
-            _debug(line)
-
-            # empty headings
-            if line.startswith(self.EMPTY_HEADING):
-                _debug('FOO_EMPTY_HEADING')
-                pass
-            # page numbers
-            elif (line.startswith(self.PAGE_NUMBER) or line.startswith(self.PAGE_BREAK)) \
-                    and self.PAGE_NUMBER_RE.match(line.replace(self.PAGE_BREAK, '')):
-                _debug('FOO_PAGE_NUMBER')
-                continue
-            # page breaks - headings
-            elif line.startswith(self.PAGE_BREAK):
-                line = line[2:]  # remove magic characters
-
-                _debug('FOO_PAGE_BREAK')
-
-                # ignore empty headings followed by the title (len == 0)
-                # and chapters with a single character per line (len == 1)
-                if len(line) < 2:
-                    continue
-
-                line = "\n{}".format(line)
-
-            # mark chapters (if they're written uppercase)
-            if line.startswith(self.PAGE_NUMBER):
-                chapter = line[1:].lstrip()
-
-                if chapter.isupper():
-                    _debug('FOO_CHAPTER - [{}]'.format(chapter))
-
-                    # ignore chapters with a single character per line (len == 1)
-                    if len(chapter) > 1:
-                        line = self._chapter_break + line[1:]
-                    else:
-                        line = self._chapter_break
-                else:
-                    continue
-
-            # cleanup
-            line = line.replace('\x1f', '')
-            # _debug(line)
-
-            # merge consecutive lines that end with a line break (-)
-            if line.endswith('-') and not line.endswith(' -'):
-                buf += line[:-1]
-            # merge lines that belong to a single sentence
-            elif not line.endswith('.'):
-                buf += line + ' '
-            else:
-                _write((buf + line).replace(" \n", "\n").rstrip())
-                buf = ''
-
-        # print any remaining lines
-        if buf != '':
-            _write(buf)
+from wbc.tidy import TextTidy
 
 
 def main():
     tidy = TextTidy(fileinput.input())
     tidy.tidy(sys.stdout)
-
-if __name__ == '__main__':
-    main()
---
+++ scripts/xml.py
@@ -0,0 +1,112 @@
+"""Generates SphinxSE-compatible XML stream with documents from a given publication / set of publications
+
+XML can then be used to index publication using SphinxSE xmlpipe2 feature
+
+Usage:
+  make_xml ID
+  make_xml (-h | --help)
+  make_xml --version
+
+Arguments:
+  ID            Publication ID (can be comma-separated list of IDs)
+
+Options:
+  -h --help     Show this screen.
+  --version     Show version.
+"""
+
+import logging
+import json
+
+from io import StringIO
+from docopt import docopt
+
+from wbc.tidy import TextTidy
+from wbc.sphinx import SphinxXML
+
+
+def get_content_stream(publication_id, issue_year, issue_id, chapter_break=''):
+    """
+    :type publication_id int
+    :type issue_id int
+    :type issue_year int
+    :type chapter_break str
+    :rtype StringIO
+    """
+    file_path = 'publications/{}/issues/{}/{}.txt'.format(publication_id, issue_year, issue_id)
+
+    output = StringIO()
+
+    with open(file_path, mode='rb') as fp:
+        TextTidy(_in=fp, chapter_break=chapter_break).tidy(output=output)
+
+    return output
+
+
+def generate():
+    """
+    Execute the script with provided arguments
+    """
+    args = docopt(__doc__, version='WBC v1.0')
+
+    chapter_break = '__CHAPTER__'
+
+    publication_ids = args['ID']
+    logging.info('Generating XML for publication(s): {}'.format(publication_ids))
+
+    xml = SphinxXML()
+
+    # schema
+
+    # fields are full-text searchable
+    xml.add_field('title')
+    xml.add_field('chapter')
+    xml.add_field('content')
+
+    # attributes are accessible via SELECT queries
+    xml.add_attr('title', 'string')
+    xml.add_attr('chapter', 'string')
+    xml.add_attr('content', 'string')
+    xml.add_attr('published_year', 'int')
+    xml.add_attr('publication_id', 'int')
+    xml.add_attr('document_id', 'int')
+
+    xml.start()
+
+    for publication_id in publication_ids.split(','):
+        # read index.json for the publication
+        index_path = 'publications/{}/index.json'.format(publication_id)
+        with open(index_path) as fp:
+            publication_data = json.load(fp)
+
+        logging.info("Got {} issues for '{}'".format(
+            publication_data['count'], publication_data['name'].encode('utf-8')))
+
+        # add documents
+        for issue in publication_data['issues']:
+            published_year = issue['year'].split('_')[-1]  # 1951_1956
+
+            try:
+                content = get_content_stream(publication_id, issue['year'], issue['id'], chapter_break=chapter_break)
+            except IOError:
+                logging.error('Failed opening an issue file', exc_info=True)
+                continue
+
+            # split by chapters and index them separately
+            chapters = content.getvalue().split(chapter_break)
+
+            for chapter in chapters:
+                chapter = chapter.strip()
+
+                xml.add_document(
+                    document_id=str(issue['id']),
+                    title=issue['name'].encode('utf-8'),
+                    chapter=chapter.split("\n")[0].strip(),
+                    content=chapter,
+                    published_year=published_year,
+                    publication_id=publication_id
+                )
+
+            content.close()
+
+    xml.end()
